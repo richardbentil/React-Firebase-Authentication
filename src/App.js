dbc038dda25bebc993691ddcbd850285aa2e8f13
richard <richard@hubtel.com>
2021-07-05 18:53:34 -0300

    add errorboundary and suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React, {Suspense} from "react"
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import ErrorBoundary from "./ErrorBoundary";
 import {
   Home,
   LoginSignUp,
@@ -7,30 +10,40 @@ import {
   SuccessOnPasswordReset,
   PageNotFound,
 } from "./routes";
+import {Spinner} from "./components/"
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/sign-in">
-          <LoginSignUp />
-        </Route>
-        <Route path="/email-sent">
-          <EmailSent />
-        </Route>
-        <Route path="/success-registration">
-          <SuccessOnRegistration />
-        </Route>
-        <Route path="/success-password-reset">
-          <SuccessOnPasswordReset />
-        </Route>
-        <Route path="*">
-          <PageNotFound />
-        </Route>
-      </Switch>
+      <ErrorBoundary>
+        <Suspense
+          fallback={
+            <div className="mx-auto">
+              <Spinner />
+            </div>
+          }>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/sign-in">
+              <LoginSignUp />
+            </Route>
+            <Route path="/email-sent">
+              <EmailSent />
+            </Route>
+            <Route path="/success-registration">
+              <SuccessOnRegistration />
+            </Route>
+            <Route path="/success-password-reset">
+              <SuccessOnPasswordReset />
+            </Route>
+            <Route path="*">
+              <PageNotFound />
+            </Route>
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
     </Router>
   );
 }
